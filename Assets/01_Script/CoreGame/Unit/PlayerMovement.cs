0999739cb8db87e8eb0f1d58cb7603425643499a
Zeprhy <lianradith@gmail.com>
2026-09-19 10:17:58 +0700

    Add : Player Movement (A & D)

---
+++ Assets/01_Script/CoreGame/Unit/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _01_Script.CoreGame.Unit
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header ("Movement Setting")]
+        [SerializeField] private float MoveSpeed = 10f;
+        private Rigidbody2D rb;
+        private Vector2 movementInput;
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        public void OnMove(InputValue value)
+        {
+            float MoveX = value.Get<float>();
+            movementInput = new Vector2(MoveX, 0f);
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 newPosition = rb.position + movementInput * MoveSpeed * Time.fixedDeltaTime;
+            rb.MovePosition(newPosition);
+        }
+    }
+}
