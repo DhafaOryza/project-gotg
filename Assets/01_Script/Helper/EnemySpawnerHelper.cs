ffe5ee72b6e5701e0cc82e896470cb3d53a7f48b
Zeprhy <lianradith@gmail.com>
2026-09-24 15:24:18 +0700

    Add : More Enemytest

--- Assets/01_Script/Helper/EnemySpawnerHelper.cs
+++ Assets/01_Script/Helper/EnemySpawnerHelper.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawnerHelper : MonoBehaviour
 {
     [Header ("Pool Config")]
-    [SerializeField] private PoolIdSO enemyPoolId;
+    [SerializeField] private List<PoolIdSO> enemyPoolId;
 
     [Header ("Spawn Settings")]
     [SerializeField] private Transform spawnPoint;
@@ -25,14 +26,20 @@ public class EnemySpawnerHelper : MonoBehaviour
         }
 
         Vector3 basePos = spawnPoint != null ? spawnPoint.position : transform.position;
-        Vector3 spawnPos = basePos + new Vector3(
+        foreach (PoolIdSO poolid in enemyPoolId)
+        {
+            if (poolid == null) continue;
+
+            Vector3 spawnPos = basePos + new Vector3(
             Random.Range(-randomOffset.x, randomOffset.x),
             Random.Range(-randomOffset.y, randomOffset.y),
             0
         );
 
-        GameObject spawnedEnemy = GameManager.Instance.poolManager.Spawn(enemyPoolId, spawnPos);
+        GameObject spawnedEnemy = GameManager.Instance.poolManager.Spawn(poolid, spawnPos);
         if (spawnedEnemy != null)
             Debug.Log($"[EnemySpawnerHelper] Berhasil spawn musuh di posisi: {spawnPos}");
+        }
+        
     }
 }
